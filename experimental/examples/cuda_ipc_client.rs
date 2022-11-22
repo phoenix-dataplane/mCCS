d637fd7e75fab60931a3b548e78a586ff4572efe
libertyeagle <wuyongji317@gmail.com>
2022-11-22 13:24:36 -0500

    init

---
+++ experimental/examples/cuda_ipc_client.rs
@@ -0,0 +1,52 @@
+use std::io::Read;
+use std::mem::size_of;
+use std::ffi::c_void;
+use std::net::TcpStream;
+
+use cuda_runtime_sys::{cudaError, cudaIpcMemHandle_t, cudaMemcpy, cudaMemcpyKind, cudaIpcOpenMemHandle, cudaIpcMemLazyEnablePeerAccess};
+
+const BUFFER_SIZE: usize = 1 * 1024 * 1024;
+
+fn main() {
+    let mut buf = vec![0i32; BUFFER_SIZE / size_of::<i32>()];
+    buf.shrink_to_fit();
+    assert_eq!(buf.capacity(), BUFFER_SIZE / size_of::<i32>());
+
+    let mut handle = cudaIpcMemHandle_t::default();
+    {
+        let mut stream = TcpStream::connect("localhost:2042").unwrap();
+        stream.set_nonblocking(false).unwrap();
+        stream.set_nodelay(true).unwrap();
+        stream.read_exact( unsafe {
+            std::slice::from_raw_parts_mut(
+                &mut handle as *mut _ as *mut u8, 
+                size_of::<cudaIpcMemHandle_t>(),
+            )
+        }).unwrap();
+    }
+    let mut dev_ptr: *mut c_void = std::ptr::null_mut();
+    let err = unsafe { cudaIpcOpenMemHandle(
+        &mut dev_ptr as *mut _, 
+        handle, 
+        cudaIpcMemLazyEnablePeerAccess) 
+    };
+    if err != cudaError::cudaSuccess {
+        panic!("cudaIpcOpenMemHandle failed")
+    }
+    let err = unsafe { 
+        cudaMemcpy(
+            buf.as_mut_ptr() as *mut _, 
+            dev_ptr,
+            BUFFER_SIZE,
+            cudaMemcpyKind::cudaMemcpyDeviceToHost,
+        )
+    };
+    if err != cudaError::cudaSuccess {
+        panic!("cudaMemcpy failed")
+    }
+
+    for x in buf.iter() {
+        assert_eq!(*x, 42, "CUDA IPC content mismatch");
+    }
+    println!("buf={}", buf[0]);
+}
---
+++ experimental/examples/cuda_ipc_server.rs
@@ -0,0 +1,51 @@
+use std::mem::size_of;
+use std::{ffi::c_void, io::Write};
+use std::net::TcpListener;
+
+use cuda_runtime_sys::{cudaMalloc, cudaError, cudaIpcMemHandle_t, cudaIpcGetMemHandle, cudaMemcpy, cudaMemcpyKind};
+
+const BUFFER_SIZE: usize = 1 * 1024 * 1024;
+
+fn main() {
+    let mut dev_ptr: *mut c_void = std::ptr::null_mut();
+    let err = unsafe { cudaMalloc(&mut dev_ptr as *mut _, BUFFER_SIZE) };
+    if err != cudaError::cudaSuccess {
+        panic!("cudaMalloc failed")
+    }
+
+    let buf = vec![42i32; BUFFER_SIZE / size_of::<i32>()];
+    let err = unsafe { 
+        cudaMemcpy(
+            dev_ptr, 
+            buf.as_ptr() as *const _, 
+            BUFFER_SIZE,
+            cudaMemcpyKind::cudaMemcpyHostToDevice,
+        )
+    };
+    if err != cudaError::cudaSuccess {
+        panic!("cudaMemcpy failed")
+    }
+
+    let mut handle = cudaIpcMemHandle_t::default();
+    let err = unsafe { cudaIpcGetMemHandle(&mut handle as *mut _, dev_ptr) };
+    if err != cudaError::cudaSuccess {
+        panic!("cudaIpcGetMemHandle failed")
+    }
+
+    let listener = TcpListener::bind("localhost:2042").unwrap();
+    match listener.accept() {
+        Ok((mut socket, addr)) => {
+            socket.write_all(
+                unsafe {
+                        std::slice::from_raw_parts(
+                        &handle as *const _ as *const u8, 
+                        size_of::<cudaIpcMemHandle_t>(),
+                        )
+                    }
+            ).unwrap();
+            println!("new client: {addr:?}")
+        },
+        Err(e) => println!("couldn't get client: {e:?}"),
+    }
+    std::thread::sleep(std::time::Duration::from_secs(2));
+}
