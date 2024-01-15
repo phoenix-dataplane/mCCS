f69a42998ad8e05b5bd230cd8883aaff3e772436
XOR-op <17672363+XOR-op@users.noreply.github.com>
2024-01-15 22:55:36 +0000

    fix: add return error check while mysteriously fix issue

--- src/mccs/src/cuda/alloc.rs
+++ src/mccs/src/cuda/alloc.rs
@@ -53,7 +53,7 @@ impl<T> DeviceHostMapped<T> {
             let mut device = 0;
             unsafe {
                 cudaGetDevice(&mut device);
-                cudaHostRegister(ptr_host as *mut _, size, cudaHostRegisterMapped);
+                cuda_warning!(cudaHostRegister(ptr_host as *mut _, size, cudaHostRegisterMapped));
                 cuda_warning!(
                     cudaHostGetDevicePointer(&mut ptr_dev, ptr_host as *mut _, 0),
                     format!("Random bug {:p}, please re-run", ptr_host)
