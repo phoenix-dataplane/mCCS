0099566766eaafb468abd05ca52a2c5ecbe0f60c
Yongji Wu <wuyongji317@gmail.com>
2024-01-26 21:03:25 +0000

    update traces

--- src/mccs_examples/traffic_gen/src/main.rs
+++ src/mccs_examples/traffic_gen/src/main.rs
@@ -1,114 +1,125 @@
 use std::net::IpAddr;
+use std::path::{Path, PathBuf};
 use std::process::ExitCode;
 use std::time::{Duration, Instant};
 
+use serde::{Deserialize, Serialize};
 use structopt::StructOpt;
 
 use cuda_runtime_sys::{cudaError, cudaMemcpyKind, cudaStream_t};
 use cuda_runtime_sys::{cudaMemcpy, cudaSetDevice};
 
+const CUDA_FLOAT16_SIZE: usize = 2;
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum OpType {
+    #[serde(alias = "all_reduce")]
+    AllReduce,
+    #[serde(alias = "all_gather")]
+    AllGather,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct OperationTrace {
+    // Message size in bytes
+    #[serde(alias = "size")]
+    message_size: usize,
+    #[serde(alias = "type")]
+    op_type: OpType,
+    // Simulated compute interval: microseconds
+    compute_interval: u64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Config {
+    // Global communicator id
+    #[serde(alias = "comm_id")]
+    communicator_id: u32,
+    // Array with num_ranks elements, rank -> cuda dev mapping
+    #[serde(alias = "cuda_devices")]
+    communicator_devices: Vec<i32>,
+    traces: Vec<OperationTrace>,
+}
+
+impl Config {
+    fn from_path<P: AsRef<Path>>(path: P) -> Config {
+        let content = std::fs::read_to_string(path).unwrap();
+        let config = toml::from_str(&content).unwrap();
+        config
+    }
+}
+
 #[derive(Debug, Clone, StructOpt)]
 #[structopt(name = "Trace traffic generator")]
 struct Opts {
-    #[structopt(long)]
+    #[structopt(long, short = "a")]
     root_addr: IpAddr,
-    #[structopt(long)]
+    #[structopt(long, short = "r")]
     rank: usize,
-    #[structopt(long)]
-    num_ranks: usize,
-    #[structopt(long)]
-    cuda_device_idx: i32,
-    #[structopt(short, long, default_value = "42")]
-    communicator: u32,
-    #[structopt(short, long, default_value = "128")]
-    size: usize,
-    #[structopt(long, default_value = "20")]
-    round: usize,
+    // Path to toml traffic trace
+    #[structopt(long, short = "c")]
+    config: PathBuf,
+    // Number of iterations
+    #[structopt(long, short = "i", default_value = "20")]
+    iters: usize,
+    #[structopt(long, short = "s")]
+    save_path: Option<PathBuf>,
+    #[structopt(long, short = "v")]
+    verbose: bool,
 }
 
 fn main() -> ExitCode {
-    let base_val = 2042;
     let opts = Opts::from_args();
-    let buffer_size = opts.size * 1024 * 1024;
     let rank = opts.rank;
-    let num_ranks = opts.num_ranks;
+    let num_iters = opts.iters;
+    let config = Config::from_path(opts.config);
+
+    let num_ranks = config.communicator_devices.len();
+    let cuda_device_idx = config.communicator_devices[rank];
+    let comm_id = config.communicator_id;
 
     unsafe {
-        let err = cudaSetDevice(opts.cuda_device_idx);
+        let err = cudaSetDevice(cuda_device_idx);
         if err != cudaError::cudaSuccess {
-            panic!("cudaSetDevice");
+            panic!("cudaSetDevice failed {:?}", err);
         }
     }
-    let dev_ptr = libmccs::cuda_malloc(opts.cuda_device_idx, buffer_size * num_ranks).unwrap();
-    let mut buf = vec![0i32; buffer_size * num_ranks / std::mem::size_of::<i32>()];
-    buf[rank * buffer_size / std::mem::size_of::<i32>()
-        ..(rank + 1) * buffer_size / std::mem::size_of::<i32>()]
-        .fill(base_val + rank as i32);
-    let err = unsafe {
-        cudaMemcpy(
-            dev_ptr.ptr,
-            buf.as_ptr() as *const _,
-            buffer_size * num_ranks,
-            cudaMemcpyKind::cudaMemcpyHostToDevice,
-        )
-    };
-    if err != cudaError::cudaSuccess {
-        panic!("cudaMemcpy failed");
-    }
-    println!("Rank {}: warm phase started", rank);
-    for r in 0..num_ranks {
-        println!(
-            "buf[{}]={}",
-            r * buffer_size / std::mem::size_of::<i32>(),
-            buf[r * buffer_size / std::mem::size_of::<i32>()]
-        );
-    }
-    let comm = libmccs::init_communicator_rank(
-        opts.communicator,
-        rank,
-        num_ranks,
-        opts.cuda_device_idx,
-        opts.root_addr,
-    )
-    .unwrap();
-    libmccs::register_stream(opts.cuda_device_idx, 0 as cudaStream_t).unwrap();
-    println!("Rank {}: start issuing", rank);
+    let traces = config.traces;
+    let mut buffer_size = traces
+        .iter()
+        .map(|x| match x.op_type {
+            OpType::AllReduce => x.message_size,
+            OpType::AllGather => x.message_size * num_ranks,
+        })
+        .max()
+        .unwrap();
+    buffer_size = buffer_size.div_ceil(num_ranks) * num_ranks;
 
-    libmccs::all_gather(
-        comm,
-        dev_ptr.add(rank * buffer_size).unwrap(),
-        dev_ptr,
-        buffer_size,
-        0 as cudaStream_t,
-    )
-    .unwrap();
-    println!("Rank {}: warmup call returned", rank);
-    let mut buf2 = vec![0; buffer_size * num_ranks / std::mem::size_of::<i32>()];
+    let dev_ptr = libmccs::cuda_malloc(cuda_device_idx, buffer_size).unwrap();
+    let buf = vec![80u8; buffer_size];
     unsafe {
         let err = cudaMemcpy(
-            buf2.as_mut_ptr() as *mut _,
             dev_ptr.ptr,
-            buffer_size * num_ranks,
-            cudaMemcpyKind::cudaMemcpyDeviceToHost,
+            buf.as_ptr() as *const _,
+            buffer_size,
+            cudaMemcpyKind::cudaMemcpyHostToDevice,
         );
         if err != cudaError::cudaSuccess {
-            panic!("cudaMemcpy failed");
+            panic!("cudaMemcpy failed {:?}", err);
         }
     };
-    for r in 0..num_ranks {
-        let data = buf2[r * buffer_size / std::mem::size_of::<i32>()];
-        let expected = base_val + r as i32;
-        if data != expected {
-            eprintln!("Rank{}: expected {}, got {}", r, expected, data);
-            return ExitCode::FAILURE;
-        }
-    }
+    let comm =
+        libmccs::init_communicator_rank(comm_id, rank, num_ranks, cuda_device_idx, opts.root_addr)
+            .unwrap();
+    libmccs::register_stream(cuda_device_idx, 0 as cudaStream_t).unwrap();
+    println!("Rank {}: start warmup", rank);
+
     for _ in 0..5 {
         libmccs::all_gather(
             comm,
-            dev_ptr.add(rank * buffer_size).unwrap(),
+            dev_ptr.add(rank * (buffer_size / num_ranks)).unwrap(),
             dev_ptr,
-            buffer_size,
+            buffer_size / num_ranks,
             0 as cudaStream_t,
         )
         .unwrap();
@@ -119,33 +130,78 @@ fn main() -> ExitCode {
             panic!("cudaStreamSynchronize failed");
         }
     }
-    println!("Rank {}: warmup phase finished", opts.rank);
-    // start testing
-    let mut start = Instant::now();
-    let mut round_times = Vec::with_capacity(opts.round);
-    for _ in 0..opts.round {
-        libmccs::all_gather(
+    println!("Rank {}: warmup phase AG finished", rank);
+
+    for _ in 0..5 {
+        libmccs::all_reduce(
             comm,
-            dev_ptr.add(rank * buffer_size).unwrap(),
             dev_ptr,
-            buffer_size,
+            dev_ptr,
+            buffer_size / CUDA_FLOAT16_SIZE,
+            libmccs::AllReduceDataType::Float16,
+            libmccs::AllReduceOpType::Sum,
             0 as cudaStream_t,
         )
         .unwrap();
-        unsafe {
-            let err = cuda_runtime_sys::cudaStreamSynchronize(0 as cudaStream_t);
-            if err != cudaError::cudaSuccess {
-                panic!("cudaStreamSynchronize failed");
+    }
+    unsafe {
+        let err = cuda_runtime_sys::cudaStreamSynchronize(0 as cudaStream_t);
+        if err != cudaError::cudaSuccess {
+            panic!("cudaStreamSynchronize failed");
+        }
+    }
+    println!("Rank {}: warmup phase finished", rank);
+
+    // start testing
+    let mut start = Instant::now();
+    let mut round_times = Vec::with_capacity(num_iters);
+    for _ in 0..num_iters {
+        for op in traces.iter() {
+            spin_sleep::sleep(Duration::from_micros(op.compute_interval));
+            match op.op_type {
+                OpType::AllReduce => {
+                    libmccs::all_reduce(
+                        comm,
+                        dev_ptr,
+                        dev_ptr,
+                        op.message_size / CUDA_FLOAT16_SIZE,
+                        libmccs::AllReduceDataType::Float16,
+                        libmccs::AllReduceOpType::Sum,
+                        0 as cudaStream_t,
+                    )
+                    .unwrap();
+                }
+                OpType::AllGather => {
+                    libmccs::all_gather(
+                        comm,
+                        dev_ptr.add(rank * (buffer_size / num_ranks)).unwrap(),
+                        dev_ptr,
+                        op.message_size,
+                        0 as cudaStream_t,
+                    )
+                    .unwrap();
+                }
+            }
+            unsafe {
+                let err = cuda_runtime_sys::cudaStreamSynchronize(0 as cudaStream_t);
+                if err != cudaError::cudaSuccess {
+                    panic!("cudaStreamSynchronize failed");
+                }
             }
         }
-        std::thread::sleep(Duration::from_millis(50));
         let end = Instant::now();
         let dura = end.duration_since(start);
         start = end;
-        let round_time = dura.as_micros() as u64 / opts.round as u64;
+        let round_time = dura.as_micros() as u64;
+        if opts.verbose && opts.rank == 0 {
+            println!("Iter time: {} ms", round_time / 1000);
+        }
         round_times.push(round_time);
     }
-    let mut wtr = csv::Writer::from_path(format!("time-{}.csv", opts.communicator)).unwrap();
-    wtr.serialize(round_times).unwrap();
+    if opts.rank == 0 && opts.save_path.is_some() {
+        let mut wtr = csv::Writer::from_path(opts.save_path.unwrap()).unwrap();
+        wtr.serialize(round_times).unwrap();
+    }
+
     return ExitCode::SUCCESS;
 }
