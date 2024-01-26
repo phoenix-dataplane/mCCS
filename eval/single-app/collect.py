78669333a45af24920172614cef2d4b5b44c22f4
XOR-op <17672363+XOR-op@users.noreply.github.com>
2024-01-26 23:17:28 +0000

    eval: scripts for running experiments and collecting data

--- eval/single-app/collect.py
+++ eval/single-app/collect.py
@@ -41,6 +41,7 @@ def rank0_parse_bw(lines: list):
     for line in lines:
         if line.find("Rank 0") != -1:
             return line.split(": ")[-1]
+    print("Error: no rank 0 found: ", lines)
 
 
 def aggregrate(base_dir: str, node_str: str):
@@ -50,19 +51,133 @@ def aggregrate(base_dir: str, node_str: str):
         for size in os.listdir(os.path.join(base_dir, node_str, command)):
             val[size] = rank0_parse_bw(
                 filter_contents(get_output(base_dir, node_str, command, size))
-            )
+            ).split(" ")[0]
         # sort by size
         ret[command] = dict(sorted(val.items(), key=lambda item: convert_size(item[0])))
     return ret
 
 
-if __name__ == "__main__":
-    # read base dir and node_str from command line
-    import sys
+def aggregrate_ecmp(n_gpu):
+    base_dir_prefix = "/tmp/single-app"
+    node_str = str(n_gpu) + "GPU_ECMP"
+    results = []
+    for i in range(10):
+        base_dir = base_dir_prefix + str(i)
+        results.append(aggregrate(base_dir, node_str))
+    for i, result in enumerate(results):
+        res = "Solution,App,Size (Bytes),Dtype,Latency (us),AlgBW (GB/s),BusBW (GB/s)\n"
+        for command in result:
+            for size in result[command]:
+                res += f"{n_gpu}GPU_ECMP,{command},{convert_size(size)},float16,0,{result[command][size]},0\n"
+        with open(f"/nfs/public/mccs-data/mccs-{n_gpu}gpu-ecmp/round{i}.csv", "w") as f:
+            f.write(res)
 
-    if len(sys.argv) != 3:
-        print("Usage: python collect.py <base_dir> <node_str>")
-        exit(1)
-    base_dir = sys.argv[1]
-    node_str = sys.argv[2]
-    print(aggregrate(base_dir, node_str))
+
+def aggregate_flow(n_gpu):
+    base_dir = "/tmp/single-app-flow"
+    node_str = str(n_gpu) + "GPU_FLOW"
+    result = aggregrate(base_dir, node_str)
+    res = "Solution,App,Size (Bytes),Dtype,Latency (us),AlgBW (GB/s),BusBW (GB/s)\n"
+    for command in result:
+        for size in result[command]:
+            res += f"{n_gpu}GPU_ECMP,{command},{convert_size(size)},float16,0,{result[command][size]},0\n"
+    with open(f"/nfs/public/mccs-data/mccs-{n_gpu}gpu-flow/round0.csv", "w") as f:
+        f.write(res)
+
+
+def pick_any():
+    if __name__ == "__main__":
+        # read base dir and node_str from command line
+        import sys
+
+        if len(sys.argv) != 3:
+            print("Usage: python collect.py <base_dir> <node_str>")
+            exit(1)
+        base_dir = sys.argv[1]
+        node_str = sys.argv[2]
+        print(aggregrate(base_dir, node_str))
+
+
+def get_all_ecmp():
+    if __name__ == "__main__":
+        # read base dir and node_str from command line
+        import sys
+
+        if len(sys.argv) != 2:
+            print("Usage: python collect.py <n_gpu>")
+            exit(1)
+        n_gpu = sys.argv[1]
+        aggregrate_ecmp(n_gpu)
+
+
+def get_all_flow():
+    if __name__ == "__main__":
+        # read base dir and node_str from command line
+        import sys
+
+        if len(sys.argv) != 2:
+            print("Usage: python collect.py <n_gpu>")
+            exit(1)
+        n_gpu = sys.argv[1]
+        aggregate_flow(n_gpu)
+
+
+def collect_all():
+    res = "Solution,App,Size (Bytes),Dtype,Latency (us),AlgBW (GB/s),BusBW (GB/s)"
+    allgather_4gpu = [res]
+    allgather_8gpu = [res]
+    allreduce_4gpu = [res]
+    allreduce_8gpu = [res]
+    for i in range(10):
+        base_dir = "/tmp/single-app" + str(i)
+        node_str = "4GPU_ECMP"
+        r = aggregrate(base_dir, node_str)
+        for command in ["allgather", "allreduce"]:
+            for size in r[command]:
+                line = f"4GPU_ECMP,{command},{convert_size(size)},float16,0,{r[command][size]},0"
+                if command == "allgather":
+                    allgather_4gpu.append(line)
+                else:
+                    allreduce_4gpu.append(line)
+        node_str = "8GPU_ECMP"
+        r = aggregrate(base_dir, node_str)
+        for command in ["allgather", "allreduce"]:
+            for size in r[command]:
+                line = f"8GPU_ECMP,{command},{convert_size(size)},float16,0,{r[command][size]},0"
+                if command == "allgather":
+                    allgather_8gpu.append(line)
+                else:
+                    allreduce_8gpu.append(line)
+    base_dir = "/tmp/single-app-flow"
+    node_str = "4GPU_FLOW"
+    r = aggregrate(base_dir, node_str)
+    for command in ["allgather", "allreduce"]:
+        for size in r[command]:
+            line = f"4GPU_FLOW,{command},{convert_size(size)},float16,0,{r[command][size]},0"
+            if command == "allgather":
+                allgather_4gpu.append(line)
+            else:
+                allreduce_4gpu.append(line)
+    node_str = "8GPU_FLOW"
+    r = aggregrate(base_dir, node_str)
+    for command in ["allgather", "allreduce"]:
+        for size in r[command]:
+            line = f"8GPU_FLOW,{command},{convert_size(size)},float16,0,{r[command][size]},0"
+            if command == "allgather":
+                allgather_8gpu.append(line)
+            else:
+                allreduce_8gpu.append(line)
+    with open(f"/nfs/public/mccs-data/mccs-allgather-4gpu.csv", "w") as f:
+        f.write("\n".join(allgather_4gpu) + "\n")
+    with open(f"/nfs/public/mccs-data/mccs-allgather-8gpu.csv", "w") as f:
+        f.write("\n".join(allgather_8gpu) + "\n")
+    with open(f"/nfs/public/mccs-data/mccs-allreduce-4gpu.csv", "w") as f:
+        f.write("\n".join(allreduce_4gpu) + "\n")
+    with open(f"/nfs/public/mccs-data/mccs-allreduce-8gpu.csv", "w") as f:
+        f.write("\n".join(allreduce_8gpu) + "\n")
+
+
+# get_all_ecmp()
+# pick_any()
+# get_all_flow()
+collect_all()
--- justfile
+++ justfile
@@ -4,30 +4,37 @@ build:
 sync:
   ./_private/sync.sh
 
+[private]
 back host LEVEL='info':
   RUST_LOG={{LEVEL}} ./target/debug/mccs --host {{host}}
 
 root_addr := '192.168.211.34'
 
+[private]
 bench rank num_ranks round='10' size='128' comm='42' cuda_dev='0':
   ./target/debug/allgather_bench --root-addr {{root_addr}} --rank {{rank}} \
   --num-ranks {{num_ranks}} --cuda-device-idx {{cuda_dev}} --size {{size}} --communicator {{comm}} --round {{round}}
 
+[private]
 allreduce_bench rank num_ranks round='10' size='128' comm='42' cuda_dev='0':
   ./target/debug/allreduce_bench --root-addr {{root_addr}} --rank {{rank}} \
   --num-ranks {{num_ranks}} --cuda-device-idx {{cuda_dev}} --size {{size}} --communicator {{comm}} --round {{round}}
 
 
+[private]
 allreduce_base rank num_ranks size='128' comm='42' cuda_dev='0':
   ./target/debug/allreduce_proto --root-addr {{root_addr}} --rank {{rank}} \
   --num-ranks {{num_ranks}} --cuda-device-idx {{cuda_dev}} --size {{size}} --communicator {{comm}}
 
+[private]
 auto-3reduce size='128' round='10' comm='42':
   just allreduce_bench $RK 3 {{round}} {{size}} {{comm}} $DEV
 
+[private]
 auto-triple size='128' round='10' comm='42':
   just bench $RK 3 {{round}} {{size}} {{comm}} $DEV
 
+[private]
 auto-double size='128' round='10' comm='42':
   just bench $RK 2 {{round}} {{size}} {{comm}} $DEV
 
@@ -39,3 +46,59 @@ kill host:
 
 killall:
   just kill 1; just kill 2; just kill 3; just kill 5
+
+[private]
+launch group folder:
+  cargo run --bin launcher -- --configfile launcher/config.toml --benchmark eval/single-app/output/ --group {{group}} --silent --output-dir /tmp/{{folder}}
+
+[private]
+one_4gpu_ecmp cnt="0":
+  just launch 4GPU_ECMP single-app{{cnt}}
+
+[private]
+one_8gpu_ecmp cnt="0":
+  just launch 8GPU_ECMP single-app{{cnt}}
+
+[private]
+one_4gpu_flow :
+  just launch 4GPU_FLOW single-app-flow
+
+[private]
+one_8gpu_flow :
+  just launch 8GPU_FLOW single-app-flow
+
+four_gpu_ecmp:
+  ./eval/single-app/set_ecmp_hashing_algo.sh everything
+  # 1 to 10
+  just one_4gpu_ecmp 0
+  just one_4gpu_ecmp 1
+  just one_4gpu_ecmp 2
+  just one_4gpu_ecmp 3
+  just one_4gpu_ecmp 4
+  just one_4gpu_ecmp 5
+  just one_4gpu_ecmp 6
+  just one_4gpu_ecmp 7
+  just one_4gpu_ecmp 8
+  just one_4gpu_ecmp 9
+
+eight_gpu_ecmp:
+  ./eval/single-app/set_ecmp_hashing_algo.sh everything
+  # 1 to 10
+  just one_8gpu_ecmp 0
+  just one_8gpu_ecmp 1
+  just one_8gpu_ecmp 2
+  just one_8gpu_ecmp 3
+  just one_8gpu_ecmp 4
+  just one_8gpu_ecmp 5
+  just one_8gpu_ecmp 6
+  just one_8gpu_ecmp 7
+  just one_8gpu_ecmp 8
+  just one_8gpu_ecmp 9  
+
+four_gpu_flow:
+  ./eval/single-app/set_ecmp_hashing_algo.sh source-port
+  just one_4gpu_flow
+
+eight_gpu_flow:
+  ./eval/single-app/set_ecmp_hashing_algo.sh source-port
+  just one_8gpu_flow
