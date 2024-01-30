08d0fb47ee2981254c5f36d3172de5358ee31b54
XOR-op <17672363+XOR-op@users.noreply.github.com>
2024-01-30 02:40:34 +0000

    eval: update dynamic figures

--- eval/multi-app/collect2.py
+++ eval/multi-app/collect2.py
@@ -71,45 +71,84 @@ def get_cdf():
         plt.title(f"{app} CDF")
 
 
+def moving_average(array, window_size):
+    smoothed_array = []
+    for i in range(len(array)):
+        window = array[max(0, i - window_size + 1) : i + 1]
+        smoothed_y = sum(point[1] for point in window) / len(window)
+        smoothed_array.append((array[i][0], smoothed_y))
+    return smoothed_array
+
+
+def get_average(data):
+    s = [i[1] for i in data if i[0] > 90000 and i[0] < 110000]
+    print(sum(s) / len(s))
+
+
 def get_trend_figure():
     vgg_path = "/tmp/dynamic-config/setup4-dynamic/traffic_gen_danyang-02.stdout"
     gpt1_path = "/tmp/dynamic-config/setup4-dynamic-gpt-1/traffic_gen_danyang-03.stdout"
     gpt2_path = "/tmp/dynamic-config/setup4-dynamic-gpt-2/traffic_gen_danyang-03.stdout"
 
     def convert_to_datetime(timestamp):
-        dt =  datetime.strptime(timestamp, "%H:%M:%S.%f")
+        dt = datetime.strptime(timestamp, "%H:%M:%S.%f")
         return (dt - datetime.min).total_seconds() * 1000
+
+    baseline = {"vgg": 344.19, "gpt_1": 26.53, "gpt_2": 26.82}
+
     def extrect(data, tag):
-        return [(convert_to_datetime(i[2]), float(i[1])) for i in data[tag]]
+        return [
+            (convert_to_datetime(i[2]), 1 / (float(i[1]) / baseline[tag]))
+            for i in data[tag]
+        ]
+        # return [(convert_to_datetime(i[2]), float(i[1])) for i in data[tag]]
 
     vgg, _ = parse_file(vgg_path)
-    vgg = extrect(vgg, 'vgg')
+    vgg = moving_average(extrect(vgg, "vgg"), 2)
     gpt1, _ = parse_file(gpt1_path)
-    gpt1 = extrect(gpt1, 'gpt_1')
+    gpt1 = moving_average(extrect(gpt1, "gpt_1"), 30)
     gpt2, _ = parse_file(gpt2_path)
-    gpt2 = extrect(gpt2, 'gpt_2')
+    gpt2 = moving_average(extrect(gpt2, "gpt_2"), 30)
     # (timestamp, throughput)
     # timestamp based on 0
     earliest_timestamp = min(min(vgg)[0], min(gpt1)[0], min(gpt2)[0])
-    aligned_vgg = [(timestamp - earliest_timestamp, throughput) for timestamp, throughput in vgg]
-    aligned_gpt1 = [(timestamp - earliest_timestamp, throughput) for timestamp, throughput in gpt1]
-    aligned_gpt2 = [(timestamp - earliest_timestamp, throughput) for timestamp, throughput in gpt2]
-    print(aligned_vgg[:10])
+    # moving average
+    aligned_vgg = [
+        (timestamp - earliest_timestamp, throughput) for timestamp, throughput in vgg
+    ]
+    aligned_gpt1 = [
+        (timestamp - earliest_timestamp, throughput) for timestamp, throughput in gpt1
+    ]
+    aligned_gpt2 = [
+        (timestamp - earliest_timestamp, throughput) for timestamp, throughput in gpt2
+    ]
+    aligned_vgg = [i for i in aligned_vgg if i[0] < 200000]
+    aligned_gpt1 = [i for i in aligned_gpt1 if i[0] < 200000]
+    aligned_gpt2 = [i for i in aligned_gpt2 if i[0] < 200000]
     plt.figure(figsize=(10, 6))
-    plt.plot([time for time, _ in aligned_vgg], [throughput for _, throughput in aligned_vgg], label='vgg')
-    plt.plot([time for time, _ in aligned_gpt1], [throughput for _, throughput in aligned_gpt1], label='gpt1')
-    plt.plot([time for time, _ in aligned_gpt2], [throughput for _, throughput in aligned_gpt2], label='gpt2')
-    plt.xlabel('Time')
-    plt.ylabel('Throughput')
-    plt.title('Throughput Over Time')
+    plt.plot(
+        [time for time, _ in aligned_vgg],
+        [throughput for _, throughput in aligned_vgg],
+        label="vgg",
+    )
+    plt.plot(
+        [time for time, _ in aligned_gpt1],
+        [throughput for _, throughput in aligned_gpt1],
+        label="gpt1",
+    )
+    plt.plot(
+        [time for time, _ in aligned_gpt2],
+        [throughput for _, throughput in aligned_gpt2],
+        label="gpt2",
+    )
+    plt.xlabel("Time")
+    plt.ylabel("Throughput")
+    plt.title("Throughput Over Time")
     plt.legend()
     plt.show()
 
-    
 
-
-
-# get_trend_figure()
-get_cdf()
+get_trend_figure()
+# get_cdf()
 
 # %%
